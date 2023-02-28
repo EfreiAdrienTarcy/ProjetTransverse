63dea0411a90544b952498acf4f0fee0d7009d55
WilfriedPonnou <75972109+WilfriedPonnou@users.noreply.github.com>
2023-02-28 11:13:56 +0100

    ocrmainfix

--- ocr/main.py
+++ ocr/main.py
@@ -9,5 +9,35 @@ images=sys.argv[1]
 
 detandrec,reconly=app_detrec.detrec(images),app_rec.rec(images)
 
+def result():
+    best_results=[]
+    for i in range(len(detandrec)):
+        
+        if detandrec[i]:
+            if reconly[i]:
+                if detandrec[i][1]>reconly[i][1]:
+                    value=detandrec[i][0]
+                    confidence=detandrec[i][1]
+                    best_results.append((value,confidence))
+                else:
+                    value=reconly[i][0]
+                    confidence=reconly[i][1]
+                    best_results.append((value,confidence))
+            else:
+                value=detandrec[i][0]
+                confidence=detandrec[i][1]
+                best_results.append((value,confidence))
+        else:
+            try:
+                value=reconly[i][0]
+                confidence=reconly[i][1]
+                best_results.append((value,confidence))
+            except:
+                print("no detection")
+                best_results.append('no card detected')
+    return best_results
+
 print("Detection and recognition: ",detandrec,"\nRecognition only: ",reconly)
+
+result()
 
