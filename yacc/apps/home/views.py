25751ab8583dc3be4d598e3612abb6ac63b109bc
EKANE <ekaneekane3@gmail.com>
2023-03-06 23:57:59 +0100

    [UPDATE] integration

--- yacc/apps/home/views.py
+++ yacc/apps/home/views.py
@@ -10,6 +10,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 from .models import *
 from .forms import ScancardForm
@@ -22,7 +24,10 @@ from mkmsdk.api_map import _API_MAP
 
 @login_required(login_url="/login/")
 def index(request):
-    context = {'segment': 'index'}
+    # Get all cards
+    cards = Cartes.objects.prefetch_related('prixhebdo_set').all()
+
+    context = {'segment': 'index', 'cards': cards}
 
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
@@ -57,7 +62,7 @@ def pages(request):
 def collections_index(request):
     # Get all cards
     cards = Cartes.objects.all()
-    context = {'cards': cards }
+    context = {'cards': cards, 'segment': 'collections' }
     html_template = "home/collections.html"
     return render(request, html_template, context)
 
@@ -88,21 +93,60 @@ def scancard(request):
             for image in ocr_result:
                 print(image[0])
                 try:
-                    api_main.card_search(image[0])
-                except:
-                    print('failure for image code' + image[0])
+                    data = api_main.card_search(image[0])
+                    print("\n\n")
+                    for cle, valeur in data.items():
+                        print(cle, valeur)
+                    tmp_price_guide = data.pop('priceGuide')
+                    print("\n\n")
+                    for cle, valeur in tmp_price_guide.items():
+                        print(cle, valeur)
+                    print("\n\n\n\n")
+
+                    print(request.user.id)
+                    carte = Cartes()
+                    prix = PrixHebdo()
+                    auth_user = AuthUser.objects.get(id=request.user.id)
+
+                    carte.num = image[0]
+                    carte.nom = data.get('locName', '')
+                    carte.image = data.get('image', '')
+                    carte.rarity = data.get('rarity', '')
+                    carte.category = data.get('categoryName', '')
+                    carte.id_user = auth_user
+                    carte.save()
+                    
+                    print("\n \n\n Carte bien enregistrée 1")
+                    prix.date_prix = timezone.now()
+                    prix.id_cartes = carte
+                    prix.sell = tmp_price_guide.get('SELL', '')
+                    prix.low = tmp_price_guide.get('LOW', '')
+                    prix.trend = tmp_price_guide.get('TREND', '')
+                    prix.lowfoil = tmp_price_guide.get('LOWFOIL', '')
+                    prix.avg_price = tmp_price_guide.get('AVG', '')
+                    prix.lowex = tmp_price_guide.get('LOWEX', '')
+                    prix.save()
+
+                    print("\n \n\n Carte bien enregistrée 2")
+
+
+                    
+                except Exception as e:
+                    print('failure for image code ' + image[0])
+                    print(e)
             
             print('Carte bien uploadée End')
+            print(image[0])
     else:
         form = ScancardForm()
     
-    context = {'form':form }
+    context = {'form':form , 'segment': 'scancard'}
     html_template = "home/scancard.html"
     return render(request, html_template, context)
 
 @login_required(login_url="/login/")
 def showcard(request,id):
     card = Cartes.objects.get(id=id)
-    context = {'card':card}
+    context = {'card':card,'segment': 'scancard' }
     html_template = "home/showcard.html"
     return render(request, html_template, context)
