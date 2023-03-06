62fcca012372a5171a95cf494916155a8baf3755
EKANE <ekaneekane3@gmail.com>
2023-03-06 14:34:16 +0100

    [ADD] integration OCR et API

---
+++ yacc/apps/home/test.py
@@ -0,0 +1,13 @@
+# Python program to explain os.getcwd() method 
+        
+# importing os module 
+import os 
+    
+# Get the current working 
+# directory (CWD) 
+cwd = os.getcwd() 
+    
+# Print the current working  
+# directory (CWD) 
+# which is root in this case
+print("Current working directory:", cwd) 
--- yacc/apps/home/views.py
+++ yacc/apps/home/views.py
@@ -1,3 +1,14 @@
+import sys, os
+# Get working directory and add ocr and API to it
+sys.path.append(os.path.join(os.getcwd(),'ocr'))
+sys.path.append(os.path.join(os.getcwd(),'API'))
+
+# Make necessary imports
+from ocr import app_detrec, app_rec, ocr_main, cropper
+from API import api_main
+from mkmsdk.mkm import Mkm
+from mkmsdk.api_map import _API_MAP
+
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
@@ -58,6 +69,22 @@ def scancard(request):
             # traitement
             #Cartes.upload_card(request.FILES['file], request)
             # form.save()
+            images = request.FILES['file']
+
+            detandrec,reconly=app_detrec.detrec(images),app_rec.rec(images)
+
+            ocr_result = ocr_main.result(images)
+
+            mkm_sandbox = Mkm(_API_MAP["2.0"]["api"], _API_MAP["2.0"]["api_sandbox_root"])
+
+            for image in ocr_result:
+                print(image[0])
+                try:
+                    api_main.card_search(image[0])
+                except:
+
+                    print('failure for image code' + image[0])
+
             print('Carte bien uploadée')
     else:
         form = ScancardForm()
